4d12f5d9bc01a1c87fe1099aea6412ab20243383
andyhu <huyifan@vip.sina.com>
2015-02-11 15:38:14 +0800

    add more proxy types for socks proxy

--- index.js
+++ index.js
@@ -40,7 +40,9 @@ exports.proxies.http = httpOrHttpsProxy;
 exports.proxies.https = httpOrHttpsProxy;
 exports.proxies.socks = socksProxy;
 exports.proxies.socks4 = socksProxy;
+exports.proxies.socks4a = socksProxy;
 exports.proxies.socks5 = socksProxy;
+exports.proxies.socks5h = socksProxy;
 
 PacProxyAgent.protocols.forEach(function (protocol) {
   exports.proxies['pac+' + protocol] = pacProxy;
